e77ba56609b74de4ae42cd9c055c05edce88e10c
Dakshi Suri <daksuri@publicisgroupe.net>
2019-09-09 19:28:51 +0530

    first push

---
+++ src/main/java/com/jenkins/jenkinspractice/JenkinsPracticeApplication.java
@@ -0,0 +1,23 @@
+package com.jenkins.jenkinspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPracticeApplication.class, args);
+	}
+	
+	
+	@GetMapping("/hello")
+	public String printMessage() {
+		
+		return "Hello World";
+	}
+
+}
